b6f784e8b01c5109d00c7f4656a9af395c2247b7
taldr27 <diegogarciaq98@gmail.com>
2024-07-12 17:39:02 -0500

    Update disclosure library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,16 @@
-import { useContext, Fragment, useState } from "react";
-import { Link, NavLink as RouterNavLink, useNavigate } from "react-router-dom";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+/* eslint-disable react/prop-types */
+import { useContext, Fragment } from "react";
+import { NavLink as RouterNavLink, useNavigate } from "react-router-dom";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
 import Badge from "@mui/material/Badge";
 import { AuthContext } from "../context/authContext";
 import { CartContext } from "../context/cartContext";
@@ -9,7 +19,6 @@ export default function Navbar() {
   const { cart } = useContext(CartContext);
   const { user, setUser } = useContext(AuthContext);
   const navigate = useNavigate();
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const closeSession = () => {
     localStorage.removeItem("accessToken");
@@ -44,29 +53,29 @@ export default function Navbar() {
                   {!user && <NavLink to="/register" text="Register" />}
                   {!user && <NavLink to="/login" text="Login" />}
                 </div>
-              </div>{" "}
+              </div>
               <div
                 className={`inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:pr-0 gap-5 ${
                   !user ? "scustom:hidden" : ""
                 } `}
               >
                 {user && (
-                  <Link
+                  <RouterNavLink
                     to="/checkout"
                     className="relative rounded-full bg-gray-800 p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                   >
                     <Badge badgeContent={cart.length} color="primary">
                       <i className="fa-solid fa-cart-shopping text-2xl text-white me-1"></i>
                     </Badge>
-                  </Link>
+                  </RouterNavLink>
                 )}
 
                 <div className="flex gap-4">
                   {user && (
                     <Menu>
-                      <Menu.Button>
+                      <MenuButton>
                         <i className="fa-solid fa-circle-user text-2xl text-white"></i>
-                      </Menu.Button>
+                      </MenuButton>
                       <Transition
                         as={Fragment}
                         enter="transition ease-out duration-100"
@@ -76,43 +85,35 @@ export default function Navbar() {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                       >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
-                          <Menu.Item>
+                        <MenuItems className="absolute right-0 z-10 mt-9 w-48 origin-top-right rounded-md p-2 bg-white shadow-lg ring-1 ring-black ring-opacity-5 hover:bg-slate-200">
+                          <MenuItem>
                             <span
-                              className="bg-gray-100 cursor-pointer ps-1 text-sm"
+                              className="cursor-pointer pl-4 text-sm w-full h-full"
                               onClick={closeSession}
                             >
                               Log Out
                             </span>
-                          </Menu.Item>
-                        </Menu.Items>
+                          </MenuItem>
+                        </MenuItems>
                       </Transition>
                     </Menu>
                   )}
-
                   <div className="inset-y-0 left-0 flex items-center scustom:hidden">
-                    <Disclosure.Button
-                      className="relative inline-flex items-center justify-center rounded-md text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-                    >
+                    <DisclosureButton className="relative inline-flex items-center justify-center rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset">
                       <span className="sr-only">Open menu</span>
-                      {mobileMenuOpen ? (
+                      {open ? (
                         <i className="fa-solid fa-xmark text-2xl"></i>
                       ) : (
                         <i className="fa-solid fa-bars text-2xl"></i>
                       )}
-                    </Disclosure.Button>
+                    </DisclosureButton>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <Disclosure.Panel className="scustom:hidden">
-            <div
-              className={`space-y-1 px-2 pb-3 pt-2 ${
-                mobileMenuOpen ? "block" : "hidden"
-              }`}
-            >
+          <DisclosurePanel className="scustom:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
               <NavLink to="/" text="Home" />
               <NavLink to="/products" text="Products" />
               {!user && <NavLink to="/register" text="Register" />}
@@ -125,25 +126,24 @@ export default function Navbar() {
                 </span>
               )}
             </div>
-          </Disclosure.Panel>
+          </DisclosurePanel>
         </>
       )}
     </Disclosure>
   );
 }
 
-function NavLink({ to, text, children }) {
+function NavLink({ to, text }) {
   return (
     <RouterNavLink
       to={to}
-      className={({ isActive }) => 
+      className={({ isActive }) =>
         `text-white block rounded-md px-3 py-2 text-sm font-medium ${
-          isActive ? "bg-gray-800" : ''
+          isActive ? "bg-gray-800" : ""
         } hover:bg-gray-700`
       }
     >
       {text}
-      {children}
     </RouterNavLink>
   );
 }
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,10 @@
+/* eslint-disable react/prop-types */
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 
 export default function ProtectedRoute({ children }) {
   const { user } = useContext(AuthContext);
-
-  console.log("protected", user);
 
   if (user) {
     return children;
